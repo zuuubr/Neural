98978e0ab3c25d34f4b91875f68de1d3e059ea36
Matyushkov Andrey <anmatyushkov@yandex.ru>
2023-04-12 19:13:40 +0300

    0.0.1v

--- Neural/main.cpp
+++ Neural/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
 	Matrix matrix_one(3, 3);
-	Matrix matrix_two(1, 3);
-	Matrix matrix_three(1, 3);
+	Matrix matrix_two(3, 3);
+	Matrix matrix_three(3, 3);
 
 	for (int i = 0; i < matrix_one.colSize(); i++) {
 		for (int j = 0; j < matrix_one.rowSize(); j++) {
@@ -12,34 +12,20 @@ int main() {
 		}
 	}
 
-	for (int i = 0; i < matrix_one.colSize(); i++) {
-		for (int j = 0; j < matrix_one.rowSize(); j++) {
-			std::cout << matrix_one.getElement(i, j) << "\t";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "====================" << std::endl;
-
 	for (int i = 0; i < matrix_two.colSize(); i++) {
 		for (int j = 0; j < matrix_two.rowSize(); j++) {
 			matrix_two.toApplyElement(i, j, i + j);
 		}
 	}
 
-	for (int i = 0; i < matrix_two.colSize(); i++) {
-		for (int j = 0; j < matrix_two.rowSize(); j++) {
-			std::cout << matrix_two.getElement(i, j) << "\t";
-		}
-		std::cout << std::endl;
-	}
+
+	matrix_one.show();
 	std::cout << "====================" << std::endl;
 
-	matrix_three = matrix_one * matrix_two;
-	for (int i = 0; i < matrix_three.colSize(); i++) {
-		for (int j = 0; j < matrix_three.rowSize(); j++) {
-			std::cout << matrix_three.getElement(i, j) << "\t";
-		}
-		std::cout << std::endl;
-	}
+	matrix_two.show();
+	std::cout << "====================" << std::endl;
+
+	matrix_three = matrix_one * 5;
+	matrix_three.show();
 	return 0;
 }
