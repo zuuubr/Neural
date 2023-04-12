f35273e798c7bf9b52fe663b5591d6a60f748196
Matyushkov Andrey <anmatyushkov@yandex.ru>
2023-04-12 19:12:41 +0300

    Добавлены перегрузки операторов для матриц, библиотека матрица зкаончена

--- Neural/matrix.cpp
+++ Neural/matrix.cpp
@@ -30,10 +30,19 @@ int Matrix::colSize() {
 	return size_x;
 }
 
-Matrix Matrix::operator+(const Matrix& right) {
+void Matrix::show() {
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			std::cout << getElement(i, j) << "\t";
+		}
+		std::cout << std::endl;
+	}
+}
+
+const Matrix Matrix::operator+(const Matrix& right) {
 	if (size_x != right.size_x || size_y != right.size_y)
 	{
-		std::cout << "Arrays of different sizes!" << std::endl;
+		std::cout << "ERROR: arrays of different sizes!" << std::endl;
 		exit(1);
 	}
 
@@ -47,10 +56,10 @@ Matrix Matrix::operator+(const Matrix& right) {
 	return result;
 }
 
-Matrix Matrix::operator+=(const Matrix& right) {
+const Matrix Matrix::operator+=(const Matrix& right) {
 	if (size_x != right.size_x || size_y != right.size_y)
 	{
-		std::cout << "Arrays of different sizes!" << std::endl;
+		std::cout << "ERROR: arrays of different sizes!" << std::endl;
 		exit(1);
 	}
 
@@ -63,9 +72,9 @@ Matrix Matrix::operator+=(const Matrix& right) {
 	return *this;
 }
 
-Matrix Matrix::operator*(const Matrix& right) {
+const Matrix Matrix::operator*(const Matrix& right) {
 	if (size_y != right.size_x) {
-		std::cout << "Arrays of different sizes!" << std::endl;
+		std::cout << "ERROR: arrays of different sizes!" << std::endl;
 		exit(1);
 	}
 
@@ -82,14 +91,14 @@ Matrix Matrix::operator*(const Matrix& right) {
 	return result;
 }
 
-Matrix Matrix::operator-(const Matrix& right) {
+const Matrix Matrix::operator-(const Matrix& right) {
 	if (size_x != right.size_x || size_y != right.size_y)
 	{
-		std::cout << "Arrays of different sizes!" << std::endl;
+		std::cout << "ERROR: arrays of different sizes!" << std::endl;
 		exit(1);
 	}
 
-	Matrix result(size_x, size_y);
+	Matrix result(size_y, size_x);
 	for (int i = 0; i < size_x; i++) {
 		for (int j = 0; j < size_y; j++) {
 			result.toApplyElement(i, j, matrix[i][j] - right.matrix[i][j]);
@@ -99,10 +108,10 @@ Matrix Matrix::operator-(const Matrix& right) {
 	return result;
 }
 
-Matrix Matrix::operator-=(const Matrix& right) {
+const Matrix Matrix::operator-=(const Matrix& right) {
 	if (size_x != right.size_x || size_y != right.size_y)
 	{
-		std::cout << "Arrays of different sizes!" << std::endl;
+		std::cout << "ERROR: arrays of different sizes!" << std::endl;
 		exit(1);
 	}
 
@@ -115,9 +124,77 @@ Matrix Matrix::operator-=(const Matrix& right) {
 	return *this;
 }
 
+const Matrix& Matrix::operator=(const Matrix& right) {
+	if (&right != this) // чтобы не выполнялось самоприсваивание
+	{
+		if (this->size_x != right.size_x || this->size_y != right.size_y)
+		{
+			for (int i = 0; i < this->size_x; i++) {
+				delete[] this->matrix[i];
+			}
+			delete[] this->matrix;
+
+			this->size_x = right.size_x; // установить нужный размер матрицы
+			this->size_y = right.size_y;
+
+			this->matrix = new float* [this->size_x];
+			for (int i = 0; i < this->size_x; i++) {
+				this->matrix[i] = new float[this->size_y];
+			}
+		}
+
+		for (int i = 0; i < this->size_x; i++)
+			for (int j = 0; j < this->size_y; j++)
+				this->matrix[i][j] = right.matrix[i][j]; // скопировать массив
+	}
+
+	return *this;
+}
+
+const Matrix Matrix::operator*(float num) {
+
+	Matrix result(this->size_y, this->size_x);
+	for (int i = 0; i < this->size_x; i++) {
+		for (int j = 0; j < this->size_y; j++) {
+			result.matrix[i][j] = this->matrix[i][j] * num;
+		}
+	}
+
+	return result;
+}
+
+const Matrix operator*(float num, const Matrix& right)
+{
+	Matrix result(right.size_y, right.size_x);
+	for (int i = 0; i < right.size_x; i++) {
+		for (int j = 0; j < right.size_y; j++) {
+			result.matrix[i][j] = right.matrix[i][j] * num;
+		}
+	}
+
+	return result;
+}
+
+const Matrix Matrix::operator/(float num) {
+	if (!num) {
+		std::cout << "ERROR: division by zero" << std::endl;
+		exit(1);
+	}
+
+	Matrix result(this->size_y, this->size_x);
+	for (int i = 0; i < this->size_x; i++) {
+		for (int j = 0; j < this->size_y; j++) {
+			result.matrix[i][j] = this->matrix[i][j] / num;
+		}
+	}
+
+	return result;
+}
+
 Matrix::~Matrix() {
 	for (int i = 0; i < size_x; i++) {
 		delete[] matrix[i];
 	}
 	delete[] matrix;
 }
+ 
