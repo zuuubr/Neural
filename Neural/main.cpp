186ae03b7affa1bf144711fd7221efed45672935
Matyushkov Andrey <anmatyushkov@yandex.ru>
2023-04-13 00:37:09 +0300

    Добавлен класс Neural

--- Neural/main.cpp
+++ Neural/main.cpp
@@ -1,31 +1,76 @@
 #include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include <time.h>
 #include "matrix.h"
 
+class Neural {
+private:
+	int inputNodes, hiddenNodes, outputNodes;
+	float speedLearning;
+	Matrix weightInputToHidden, weightHiddenToOutput;
+public:
+	Neural(int, int, int, float = 0.3);
+	inline float sigma(float num) { return (1 / (1 + exp(-num))); }
+	const Matrix query(Matrix&);
+};
+
+Neural::Neural(int _inputNodes, int _hiddenNodes, int _outputNodes, float _speedLearning) {
+	srand(time(NULL));
+	float randomValue;
+
+	inputNodes = _inputNodes;
+	hiddenNodes = _hiddenNodes;
+	outputNodes = _outputNodes;
+	speedLearning = _speedLearning;
+
+	// Создадим 2 матрицы весовых коэффициентов
+	// от входного слоя до скрытого
+	// от скрытого слоя до выходного
+
+	Matrix bufWeightInputToHidden(hiddenNodes, inputNodes), bufWeightHiddenToOutput(outputNodes, hiddenNodes);
+	for (int i = 0; i < bufWeightInputToHidden.colSize(); i++) {
+		for (int j = 0; j < bufWeightInputToHidden.rowSize(); j++) {
+			randomValue = rand() % (int)pow(10, 2);
+			randomValue = -1 + (randomValue / pow(10, 2)) * 2;
+			bufWeightInputToHidden.toApplyElement(i, j, randomValue);
+		}
+	}
+
+	for (int i = 0; i < bufWeightHiddenToOutput.colSize(); i++) {
+		for (int j = 0; j < bufWeightHiddenToOutput.rowSize(); j++) {
+			randomValue = rand() % (int)pow(10, 2);
+			randomValue = -1 + (randomValue / pow(10, 2)) * 2;
+			bufWeightHiddenToOutput.toApplyElement(i, j, randomValue);
+		}
+	}
+
+	weightInputToHidden = bufWeightInputToHidden;
+	weightHiddenToOutput = bufWeightHiddenToOutput;
+}
+
+const Matrix Neural::query(Matrix& inputs) {
+	Matrix hiddenInputs, hiddenOutputs, finalInputs, finalOutputs;
+	inputs = inputs.transponse();
+
+	hiddenInputs = weightInputToHidden * inputs;
+	for (int i = 0; i < hiddenInputs.colSize(); i++) {
+		for (int j = 0; j < hiddenInputs.rowSize(); j++) {
+			hiddenOutputs.toApplyElement(i, j, sigma(hiddenInputs.getElement(i, j)));
+		}
+	}
+
+	finalInputs = weightHiddenToOutput * hiddenOutputs;
+	for (int i = 0; i < finalInputs.colSize(); i++) {
+		for (int j = 0; j < finalInputs.rowSize(); j++) {
+			finalOutputs.toApplyElement(i, j, sigma(finalInputs.getElement(i, j)));
+		}
+	}
+
+	return finalInputs;
+}
+
 int main() {
-	Matrix matrix_one(3, 3);
-	Matrix matrix_two(3, 3);
-	Matrix matrix_three(3, 3);
-
-	for (int i = 0; i < matrix_one.colSize(); i++) {
-		for (int j = 0; j < matrix_one.rowSize(); j++) {
-			matrix_one.toApplyElement(i, j, i + j);
-		}
-	}
-
-	for (int i = 0; i < matrix_two.colSize(); i++) {
-		for (int j = 0; j < matrix_two.rowSize(); j++) {
-			matrix_two.toApplyElement(i, j, i + j);
-		}
-	}
-
-
-	matrix_one.show();
-	std::cout << "====================" << std::endl;
-
-	matrix_two.show();
-	std::cout << "====================" << std::endl;
-
-	matrix_three = matrix_one * 5;
-	matrix_three.show();
+	Neural network(3, 2, 3);
 	return 0;
 }
